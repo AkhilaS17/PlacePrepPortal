b6f377b3c7b4f1df0a6e410a6817ac5bbcf81c9d
Akhila Sreeperumburu <sakhila1706@gmail.com>
2025-10-29 17:50:24 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";  // ✅ Import here
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Alumni from "./components/Alumni";
 import References from "./components/References";
-// import Notifications from "./components/Notifications";
-import Footer from "./components/Footer";
+import Login from "./components/Login";
+import AdminDashboard from "./pages/AdminDashboard";
+import StudentDashboard from "./pages/StudentDashboard";
+
 function App() {
   return (
     <Router>
       <Navbar />
-
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/student-dashboard" element={<StudentDashboard />} />
         <Route path="/alumni" element={<Alumni />} />
         <Route path="/references" element={<References />} />
-        {/* <Route path="/notifications" element={<Notifications />} /> */}
       </Routes>
-
       <Footer />
     </Router>
   );
