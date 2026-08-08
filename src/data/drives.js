2300c334a989c3ef912cc29497889250fca5aef4
Akhila S <sakhila1706@gmail.com>
2026-08-08 18:44:48 +0530

    updated drives data

--- src/data/drives.js
+++ src/data/drives.js
@@ -1,21 +1,29 @@
 // src/data/drives.js
 export const drives = [
+   {
+    company: "Cognizant",
+    date: "2026-08-14",
+    eligibility: "CGPA ≥ 7.0 | CSE, ECE, IT",
+    url: "https://careers.cognizant.com/india-en/"
+  },
   {
     company: "Infosys",
-    date: "2025-10-15",
+    date: "2026-08-18",
     eligibility: "CGPA ≥ 7.0 | CSE, ECE",
     url: "https://www.infosys.com/careers/"
   },
-  {
-    company: "Deloitte",
-    date: "2025-10-20",
-    eligibility: "CGPA ≥ 7.0 | CSE, ECE, IT",
-    url: "https://www2.deloitte.com/global/en/careers"
-  },
+  
   {
     company: "TCS",
-    date: "2025-10-25",
+    date: "2026-08-25",
     eligibility: "CGPA ≥ 6.5 | All Branches",
     url: "https://www.tcs.com/careers"
+  },
+  {
+    company: "Deloitte",
+    date: "2026-09-20",
+    eligibility: "CGPA ≥ 7.0 | CSE, ECE, IT",
+    url: "https://www2.deloitte.com/global/en/careers"
   }
+
 ];
